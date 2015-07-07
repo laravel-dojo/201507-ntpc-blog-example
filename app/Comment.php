77d6a8c0fbc39cdf6d3f56a9079a6a0fdb708f27
Shengyou Fan <shengyoufan@gmail.com>
2015-07-08 01:55:50 +0800

    更新：設定 Model fillable

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+    	'name',
+    	'email',
+    	'content',
+    ];
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+    	'title',
+    	'sub_title',
+    	'content',
+    	'is_feature',
+    	'page_view',
+    ];
 }
